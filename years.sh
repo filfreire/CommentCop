9380c6f53c155a8b98da895e2a2618a0ff1c69f1
Filipe Freire <livrofubia@gmail.com>
2018-12-01 19:33:05 +0100

    #49 - fix date statement format

--- years.sh
+++ years.sh
@@ -3,7 +3,7 @@
 if (grep -r "Copyright \+(c) \+2017-.*Filipe Freire" \
   --exclude-dir "target" --exclude-dir ".git" --exclude "years.sh" \
   --exclude-dir ".idea" --exclude "settings.xml" \
-  --exclude-dir "apache-maven-*" . | grep -v 2017-`date +%Y`); then
+  --exclude-dir "apache-maven-*" . | grep -v 2017-$(date +%Y)); then
     echo "Files above have wrong years in copyrights"
     exit 1
 fi
@@ -13,7 +13,7 @@ if (grep -r -L "Copyright \+(c) \+2017-.*Filipe Freire" \
   --include=*.txt --include=*.fml --include=*.properties} \
   --exclude-dir "target" --exclude-dir "apache-maven-*" . \
   --exclude-dir ".idea" --exclude "settings.xml" \
-  | grep -v 2017-`date +%Y`); then
+  | grep -v 2017-$(date +%Y)); then
     echo "Files above must have copyright block in header"
     exit 1
 fi
